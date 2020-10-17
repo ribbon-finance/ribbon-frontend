f1ddd3d78b1338d34b410fc848a05cf1f1a0f94a
kenchangh <guanhao3797@gmail.com>
2020-10-18 00:25:19 +0100

    more

--- mvp/src/protocol/contracts.ts
+++ mvp/src/protocol/contracts.ts
@@ -1,10 +1,12 @@
-import { Web3JsProvider } from "ethereum-types";
+import { Web3JsV2Provider } from "ethereum-types";
 import Web3 from "web3";
-import web3 from "../utils/web3";
 import addresses from "./addresses.json";
 import { DojimaInstrumentContract } from "./codegen/dojima_instrument";
 
+const infuraURL = "https://kovan.infura.io/v3/fbe1acc69f4e47fb83ef075cf795cf54";
+const provider = new Web3.providers.HttpProvider(infuraURL);
+
 export const dojimaInstrument = new DojimaInstrumentContract(
   addresses.contracts.dojimaInstrument,
-  web3.currentProvider as Web3JsProvider
+  provider as Web3JsV2Provider
 );
