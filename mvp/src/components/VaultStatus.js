458c9f8438bd3f5f6a1e3e87b792ee780eed027d
kenchangh <guanhao3797@gmail.com>
2020-10-18 01:40:33 +0100

    reading col ratio

--- mvp/src/components/VaultStatus.js
+++ mvp/src/components/VaultStatus.js
@@ -1,10 +1,25 @@
-import React, { useContext } from "react";
-import { Web3Context } from "./Web3Wrapper";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import { useContracts } from "../utils/contracts";
 
 const VaultStatus = () => {
-  const context = useContext(Web3Context);
-  console.log(context);
-  return <div>Min Collateralization Ratio:</div>;
+  const contracts = useContracts();
+  const [colRatio, setColRatio] = useState(0.0);
+
+  useEffect(() => {
+    if (contracts !== null && contracts.dojimaInstrument) {
+      (async () => {
+        const colRatio = await contracts.dojimaInstrument.methods
+          .collateralizationRatio()
+          .call();
+
+        const colRatioEther = parseFloat(Web3.utils.fromWei(colRatio));
+        setColRatio(colRatioEther * 100);
+      })();
+    }
+  }, [contracts]);
+
+  return <div>Min Collateralization Ratio: {colRatio}%</div>;
 };
 
 export default VaultStatus;
