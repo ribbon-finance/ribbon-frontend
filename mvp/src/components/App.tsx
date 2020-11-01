6e1d2064a4df49b44b7519f51b9d4d3c65565ad3
kenchangh <guanhao3797@gmail.com>
2020-11-01 15:41:31 +0000

    vscode

--- mvp/src/components/App.tsx
+++ mvp/src/components/App.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>$399.20</div>
+    </header>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
