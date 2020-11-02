a44d8458bb8b5e96640ae922fc4fbfcf7d2c4907
kenchangh <guanhao3797@gmail.com>
2020-11-02 18:39:21 +0000

    add ethers web3

--- mvp/src/components/App.tsx
+++ mvp/src/components/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Web3ReactProvider } from "@web3-react/core";
+// import your favorite web3 convenience library here
+
 import Header from "./Header";
 import ProductListing from "./ProductListing";
 import PurchaseInstrument from "./PurchaseInstrument";
 import { products } from "../mockData";
 import Content404 from "./Content404";
+import getLibrary from "../utils/getLibrary";
 
 const AppContainer = styled.div`
   padding: 23px 30px;
@@ -15,24 +19,26 @@ const MainContent = styled.div``;
 
 function App() {
   return (
-    <Router>
-      <AppContainer className="App">
-        <Header />
-        <MainContent>
-          <Switch>
-            <Route exact path="/">
-              <ProductListing product={products[0]}></ProductListing>
-            </Route>
-            <Route exact path="/instrument/:instrumentSymbol">
-              <PurchaseInstrument></PurchaseInstrument>
-            </Route>
-            <Route path="*">
-              <Content404></Content404>
-            </Route>
-          </Switch>
-        </MainContent>
-      </AppContainer>
-    </Router>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Router>
+        <AppContainer className="App">
+          <Header />
+          <MainContent>
+            <Switch>
+              <Route exact path="/">
+                <ProductListing product={products[0]}></ProductListing>
+              </Route>
+              <Route exact path="/instrument/:instrumentSymbol">
+                <PurchaseInstrument></PurchaseInstrument>
+              </Route>
+              <Route path="*">
+                <Content404></Content404>
+              </Route>
+            </Switch>
+          </MainContent>
+        </AppContainer>
+      </Router>
+    </Web3ReactProvider>
   );
 }
 
