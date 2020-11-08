a3d87f9a4ccaf3e9344233b1fd9019a26649e477
kenchangh <guanhao3797@gmail.com>
2020-11-08 03:02:55 +0000

    add dai

--- mvp/src/mockData.tsx
+++ mvp/src/mockData.tsx
@@ -1,28 +1,12 @@
 import { Product } from "./models";
+import instruments from "./constants/instruments.json";
 
 export const products: Product[] = [
   {
     name: "Twin Yield ETH-USD",
     targetCurrency: "ETH",
-    paymentCurrency: "USDC",
-    expiryTimestamp: 1604361600,
-    instruments: [
-      {
-        symbol: "TY-ETHUSDC-390-03112020",
-        instrumentSpotPrice: 380,
-        targetSpotPrice: 399.2,
-        strikePrice: 390,
-        expiryTimestamp: 1604361600,
-        balancerPool: "0x75286e183D923a5F52F52be205e358c5C9101b09",
-      },
-      {
-        symbol: "TY-ETHUSDC-400-03112020",
-        instrumentSpotPrice: 390,
-        targetSpotPrice: 399.2,
-        strikePrice: 400,
-        expiryTimestamp: 1604361600,
-        balancerPool: "0x75286e183D923a5F52F52be205e358c5C9101b09",
-      },
-    ],
+    paymentCurrency: "DAI",
+    expiryTimestamp: instruments.kovan[0].expiry,
+    instruments: [],
   },
 ];
