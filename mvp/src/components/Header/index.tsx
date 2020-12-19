35d55535d512d38a035c5cb88978bba2839dd738
Julian Koh <jk2698@cornell.edu>
2020-12-19 22:59:09 +0000

    new header

--- mvp/src/components/Header/index.tsx
+++ mvp/src/components/Header/index.tsx
@@ -1,44 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import { Row, Col } from 'antd';
 import AccountStatus from "./AccountStatus";
 import CurrentPrice from "./CurrentPrice";
 import Logo from "./Logo";
 
-const HeaderContainer = styled.div`
+const Content = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-evenly;
-  width: 100%;
-  height: 5em;
+  justify-content: center;
 `;
-
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const HeaderMiddle = styled.div``;
-
-const HeaderRight = styled.div``;
 
 const Header = () => {
   return (
-    <header>
-      <HeaderContainer>
-        <HeaderLeft>
-          <CurrentPrice></CurrentPrice>
-        </HeaderLeft>
-
-        <HeaderMiddle>
-          <Logo></Logo>
-        </HeaderMiddle>
-
-        <HeaderRight>
-          <AccountStatus></AccountStatus>
-        </HeaderRight>
-      </HeaderContainer>
-    </header>
+    <Row align="middle">
+      <Col span={4} offset={6}>
+        <Content><CurrentPrice></CurrentPrice></Content>
+      </Col>
+      <Col span={4}>
+        <Content><Logo></Logo></Content>
+      </Col>
+      <Col span={4}>
+        <Content><AccountStatus></AccountStatus></Content>
+      </Col>
+      <Col span={6}></Col>
+    </Row>
   );
 };
 
