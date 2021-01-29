33196db3a4da2d4d3b85daa09490c844b1946b7f
kenchangh <guanhao3797@gmail.com>
2021-01-29 23:37:14 +0800

    fix alignment

--- mvp/src/components/Categories/CategoryCard.tsx
+++ mvp/src/components/Categories/CategoryCard.tsx
@@ -4,9 +4,32 @@ import { Row, Col, Card } from "antd";
 import { LineChartOutlined } from "@ant-design/icons";
 
 export const StyledCard = styled(Card)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border-radius: 10px;
   background-color: #f5f5f5;
   width: 22%;
+`;
+const SplitRow = styled(Row)`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+const StyledCol = styled(Col)`
+  display: flex;
+  align-items: center;
+`;
+const AlignLeftCol = styled(StyledCol)`
+  text-align: left;
+  justify-content: flex-start;
+`;
+const AlignRightCol = styled(StyledCol)`
+  justify-content: flex-end;
+`;
+const CardLineChart = styled(LineChartOutlined)`
+  font-size: 20px;
+  height: 100%;
 `;
 
 type Props = {
@@ -23,15 +46,15 @@ function iconPicker(icon: string) {
 
 const CategoryCard: React.FC<Props> = ({ text, icon }) => {
   return (
-    <StyledCard hoverable>
-      <Row style={{ textAlign: "left" }} align="middle">
-        <Col span={16}>
+    <StyledCard bodyStyle={{ width: "100%" }} hoverable>
+      <SplitRow align="middle">
+        <AlignLeftCol span={16}>
           <span>{text}</span>
-        </Col>
-        <Col span={8}>
-          <LineChartOutlined style={{ fontSize: 20, height: "100%" }} />
-        </Col>
-      </Row>
+        </AlignLeftCol>
+        <AlignRightCol span={8}>
+          <CardLineChart />
+        </AlignRightCol>
+      </SplitRow>
     </StyledCard>
   );
 };
