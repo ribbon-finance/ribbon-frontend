82a5384a2ec722783a601b64a46e910e6897322d
kenchangh <guanhao3797@gmail.com>
2021-02-03 14:37:49 +0800

    move strict mode away

--- mvp/src/components/App.tsx
+++ mvp/src/components/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import styled from "styled-components";
 import { Row, Col } from "antd";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Web3ReactProvider } from "@web3-react/core";
+import { createWeb3ReactRoot, Web3ReactProvider } from "@web3-react/core";
 // import your favorite web3 convenience library here
 
 import Header from "./Header";
@@ -19,42 +19,48 @@ const MainContent = styled.div`
 `;
 
 function App() {
+  const Web3ReactInjected = createWeb3ReactRoot("injected");
+
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Router>
-        <AppContainer className="App">
-          <Header />
-          <MainContent>
-            <Row align="middle">
-              <Col
-                md={{ span: 18, offset: 3 }}
-                lg={{ span: 18, offset: 3 }}
-                xl={{ span: 18, offset: 3 }}
-                xxl={{ span: 12, offset: 6 }}
-              >
-                <Switch>
-                  <Route path="/" exact>
-                    <HomePage></HomePage>
-                  </Route>
-                  <Route path="/product/:categoryID">
-                    <HomePage></HomePage>
-                  </Route>
-                  <Route exact path="/instrument/:instrumentSymbol">
-                    <Product></Product>
-                  </Route>
-                  <Route exact path="/portfolio">
-                    <Portfolio></Portfolio>
-                  </Route>
-                  <Route path="*">
-                    <Content404></Content404>
-                  </Route>
-                </Switch>
-              </Col>
-              <Col span={3}></Col>
-            </Row>
-          </MainContent>
-        </AppContainer>
-      </Router>
+      <Web3ReactInjected getLibrary={getLibrary}>
+        <StrictMode>
+          <Router>
+            <AppContainer className="App">
+              <Header />
+              <MainContent>
+                <Row align="middle">
+                  <Col
+                    md={{ span: 18, offset: 3 }}
+                    lg={{ span: 18, offset: 3 }}
+                    xl={{ span: 18, offset: 3 }}
+                    xxl={{ span: 12, offset: 6 }}
+                  >
+                    <Switch>
+                      <Route path="/" exact>
+                        <HomePage></HomePage>
+                      </Route>
+                      <Route path="/product/:categoryID">
+                        <HomePage></HomePage>
+                      </Route>
+                      <Route exact path="/instrument/:instrumentSymbol">
+                        <Product></Product>
+                      </Route>
+                      <Route exact path="/portfolio">
+                        <Portfolio></Portfolio>
+                      </Route>
+                      <Route path="*">
+                        <Content404></Content404>
+                      </Route>
+                    </Switch>
+                  </Col>
+                  <Col span={3}></Col>
+                </Row>
+              </MainContent>
+            </AppContainer>
+          </Router>
+        </StrictMode>
+      </Web3ReactInjected>
     </Web3ReactProvider>
   );
 }
