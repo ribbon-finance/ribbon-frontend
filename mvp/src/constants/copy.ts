4112ec0a3d1d16c02b9e3fe5424082293c213297
Julian Koh <jk2698@cornell.edu>
2021-02-04 01:35:29 +0000

    copy for 4 products

--- mvp/src/constants/copy.ts
+++ mvp/src/constants/copy.ts
@@ -11,7 +11,7 @@ export const CATEGORIES: Record<string, Record<string, string>> = {
     cardTitle: "Enhanced Yields",
     icon: "lineChart",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat vestibulum lacus, id vulputate dolor venenatis vel. Donec sit amet libero tempus, ultricies ante quis, pharetra nisi. Nulla sed pharetra tortor. Nunc pulvinar erat ultrices nisl porttitor tristique ut consectetur urna. Ut maximus, dui non finibus bibendum, magna diam pellentesque neque, quis tincidunt leo tortor ut purus. Duis sed rutrum dolor. Nullam in justo at lacus dignissim varius a vitae felis. Nunc malesuada dapibus diam vitae viverra. Donec rutrum velit nec aliquet euismod. Quisque pulvinar, tortor sed elementum faucibus, orci ligula convallis orci, eget placerat mi libero sollicitudin lorem.",
+      "Enhance your yields by selling options, collateralized by interest-bearing assets. Stack your yields with option premiums + interest. ",
   },
   "principal-protection": {
     cardColor: "#B1C9B6",
@@ -19,13 +19,13 @@ export const CATEGORIES: Record<string, Record<string, string>> = {
     cardTitle: "Principal Protection",
     icon: "lineChart",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat vestibulum lacus, id vulputate dolor venenatis vel. Donec sit amet libero tempus, ultricies ante quis, pharetra nisi. Nulla sed pharetra tortor. Nunc pulvinar erat ultrices nisl porttitor tristique ut consectetur urna. Ut maximus, dui non finibus bibendum, magna diam pellentesque neque, quis tincidunt leo tortor ut purus. Duis sed rutrum dolor. Nullam in justo at lacus dignissim varius a vitae felis. Nunc malesuada dapibus diam vitae viverra. Donec rutrum velit nec aliquet euismod. Quisque pulvinar, tortor sed elementum faucibus, orci ligula convallis orci, eget placerat mi libero sollicitudin lorem.",
+      "Get paid double-digit APY yields, or get back your principal, if the price of ETH crosses some threshold. Ribbon constructs this through combining zero-coupon bounds and options, and guarantees that your principal is never at risk.",
   },
   accumulation: {
     cardColor: "#5AB1DA",
     cardTitle: "Accumulation",
     icon: "lineChart",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat vestibulum lacus, id vulputate dolor venenatis vel. Donec sit amet libero tempus, ultricies ante quis, pharetra nisi. Nulla sed pharetra tortor. Nunc pulvinar erat ultrices nisl porttitor tristique ut consectetur urna. Ut maximus, dui non finibus bibendum, magna diam pellentesque neque, quis tincidunt leo tortor ut purus. Duis sed rutrum dolor. Nullam in justo at lacus dignissim varius a vitae felis. Nunc malesuada dapibus diam vitae viverra. Donec rutrum velit nec aliquet euismod. Quisque pulvinar, tortor sed elementum faucibus, orci ligula convallis orci, eget placerat mi libero sollicitudin lorem.",
+      "Earn double-digit APY yields on the way up, buy the dip on the way down. Automatically accumulate more ETH on every dip, and get paid high yields when ETH goes up.",
   },
 };
