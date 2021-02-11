f387ce09102a1cbc98dae685e9a1098cd54613a5
kenchangh <guanhao3797@gmail.com>
2021-02-11 17:36:32 +0800

    fix nav

--- mvp/src/components/Header/index.tsx
+++ mvp/src/components/Header/index.tsx
@@ -18,7 +18,7 @@ const Content = styled.div`
 
 const Navigation = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
 `;
 
 type NavLinkProps = {
@@ -30,24 +30,17 @@ const LinkText = styled(PrimaryMedium)`
   font-weight: 500;
 `;
 
-const NavLink = ({ link, text }: NavLinkProps) => (
-  <Link to={link}>
-    <LinkText>{text}</LinkText>
-  </Link>
-);
-
 const Header = () => {
   const { loading: loadingPositions, numOfActivePositions } = usePositions();
   const { active } = useWeb3React();
   let positionsNav;
   if (active) {
     positionsNav = (
-      <NavLink
-        link="/portfolio"
-        text={`Positions${
+      <Link to="/portfolio">
+        <LinkText>{`Positions${
           loadingPositions ? "" : ` (${numOfActivePositions})`
-        }`}
-      />
+        }`}</LinkText>
+      </Link>
     );
   } else {
     positionsNav = null;
@@ -63,8 +56,13 @@ const Header = () => {
         </Col>
         <Col span={5} offset={3}>
           <Navigation>
-            <NavLink link="/" text="FAQ" />
-            <a href="https://medium.com/@ribbonfinance">
+            <Link to="/faq" style={{ marginLeft: 30, marginRight: 30 }}>
+              <LinkText>FAQ</LinkText>
+            </Link>
+            <a
+              style={{ marginLeft: 30, marginRight: 30 }}
+              href="https://medium.com/@ribbonfinance"
+            >
               <LinkText>Blog</LinkText>
             </a>
           </Navigation>
