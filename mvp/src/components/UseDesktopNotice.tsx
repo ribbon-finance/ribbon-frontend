2c161bedc8a06bb66f12f397c31523c7eeedd754
Julian Koh <jk2698@cornell.edu>
2021-02-12 15:24:48 +0000

    Revert "mobile WIP"
    This reverts commit c16999c82f0f11de0d475bd32062298183e123bc.

--- mvp/src/components/UseDesktopNotice.tsx
+++ mvp/src/components/UseDesktopNotice.tsx
@@ -1,26 +1,8 @@
-import styled from "styled-components";
-import { Row, Col } from "antd";
-import Ribbon from "../img/RibbonLogo.svg";
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background-image: url(https://i.pinimg.com/originals/90/3b/af/903baf04a5ea6a85105d2fc6e336304d.jpg);
-  background-repeat: no-repeat;
-  background-position: center center;
-`;
-
 const UseDesktopNotice = () => {
   return (
-    <Container>
-      <Row align="middle" justify="center" style={{ height: "100%" }}>
-        <img
-          src={Ribbon}
-          alt="Ribbon Finance"
-          style={{ height: 60, width: 60 }}
-        ></img>
-      </Row>
-    </Container>
+    <div>
+      This website does not support mobile devices yet. Please use a desktop.
+    </div>
   );
 };
 export default UseDesktopNotice;
