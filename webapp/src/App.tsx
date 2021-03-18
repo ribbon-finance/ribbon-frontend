30873c48384af39cb91744bcdeda19d41a3884e6
Ken Chan <guanhao3797@gmail.com>
2021-03-19 00:37:05 +0800

    webapp init (#55)

---
+++ webapp/src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.less";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+          <Button>test</Button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
