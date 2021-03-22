eda5ed47e2a513eea98711d240c4911d5589f877
Chan Jing Hong <cjinghong@users.noreply.github.com>
2021-03-22 21:48:21 +0800

    Jh/Webapp fix header style (#60)
    * Fix header not properly centered issue
    
    * Update hover effect on navigation item
    
    * Remove unnecessary console log
    
    * Remove react-native type dependencies

--- webapp/src/components/Header/index.tsx
+++ webapp/src/components/Header/index.tsx
@@ -1,19 +1,21 @@
 import Logo from "./Logo";
 import colors from "../../designSystem/colors";
 import { mobileWidth } from "../../designSystem/sizes";
-import { BaseText } from "../../designSystem";
+import { BaseText, BaseLink } from "../../designSystem";
 import Indicator from "../Indicator/Indicator";
 import MenuButton from "./MenuButton";
+import { NavItemProps, MobileOverlayMenuProps } from "./types";
 
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+
 import { useState } from "react";
 
 const HEADER_HEIGHT = 80;
 
 const HeaderContainer = styled.div`
   height: ${HEADER_HEIGHT}px;
-	border-bottom: 1px solid ${colors.border};
+  border-bottom: 1px solid ${colors.border};
+  position: relative;
 
   @media (max-width: ${mobileWidth}px) {
     padding: 16px 24px;
@@ -22,12 +24,12 @@ const HeaderContainer = styled.div`
     left: 0;
     right: 0;
     border-bottom: none;
-    background-color: ${colors.background}
+    background-color: ${colors.background};
   }
 `;
 
 const LinkText = styled(BaseText)`
-	font-family: VCR;
+  font-family: VCR;
   font-weight: 500;
   font-size: 16px;
   color: white;
@@ -35,7 +37,7 @@ const LinkText = styled(BaseText)`
   @media (max-width: ${mobileWidth}px) {
     font-size: 24px;
   }
-`
+`;
 
 const LogoContainer = styled.div`
   padding-left: 40px;
@@ -43,42 +45,57 @@ const LogoContainer = styled.div`
   @media (max-width: ${mobileWidth}px) {
     padding-left: 0;
   }
-`
-const LinksContainer = styled.div`
+`;
+
+const HeaderAbsoluteContainer = styled.div`
+  position: absolute;
   display: flex;
-  
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+
   @media (max-width: ${mobileWidth}px) {
     display: none;
   }
-`
+`;
+
+const LinksContainer = styled.div`
+  display: flex;
+`;
 
 const VerticalSeparator = styled.div`
-	border-left: 1px solid ${colors.border};
-`
+  border-left: 1px solid ${colors.border};
+`;
 
-const NavItem = styled.div`
-	padding: 28px;
-	opacity: 0.48;
-`
+const NavItem = styled.div<NavItemProps>`
+  padding: 28px;
+  opacity: ${(props) => (props.isSelected ? "1" : "0.48")};
+
+  &:hover {
+    opacity: ${(props) => (props.isSelected ? "0.85" : "1")};
+  }
+`;
 
 const WalletContainer = styled(VerticalSeparator)`
   display: flex;
   justify-content: center;
   align-items: center;
-	padding: 28px;
+  padding: 28px;
 
   @media (max-width: ${mobileWidth}px) {
     display: none;
   }
-`
+`;
 
 const MobileOnly = styled.div`
-  @media (min-width: ${mobileWidth}px) {
-    display: none;
-  }
-`
+  display: none;
 
-const MobileOverlayMenu = styled.div`
+  @media (max-width: ${mobileWidth}px) {
+    display: flex;
+  }
+`;
+
+const MobileOverlayMenu = styled.div<MobileOverlayMenuProps>`
   position: fixed;
   top: 0;
   left: 0;
@@ -87,38 +104,37 @@ const MobileOverlayMenu = styled.div`
   z-index: -1;
   backdrop-filter: blur(80px);
   transition: 0.1s all ease-in;
-`
+
+  ${(props) =>
+    props.isMenuOpen
+      ? `
+    opacity: 1;
+  `
+      : `
+    opacity: 0;
+    visibility: hidden;
+  `}
+`;
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const onToggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const renderLinkItem = (title: string, to: string, isSelected: boolean) => {
-    const itemStyle = {
-      opacity: isSelected ? 1 : 0.48
-    }
     return (
-      <Link to={to}>
-        <NavItem style={itemStyle}>
+      <BaseLink to={to}>
+        <NavItem isSelected={isSelected}>
           <LinkText>{title}</LinkText>
         </NavItem>
-      </Link>
-    )
-  }
+      </BaseLink>
+    );
+  };
 
   // TODO: - Change to actual wallet status
-  const walletConnected = true
-  const mobileOverlayStyle: React.CSSProperties = isMenuOpen
-    ? {
-      opacity: 1
-    }
-    : {
-      visibility: 'hidden',
-      opacity: 0
-    }
+  const walletConnected = true;
 
   return (
     <HeaderContainer className="d-flex align-items-center justify-content-between">
@@ -128,33 +144,34 @@ const Header = () => {
       </LogoContainer>
 
       {/* LINKS */}
-      <LinksContainer>
-        <VerticalSeparator />
-        {renderLinkItem('PRODUCTS', '/', true)}
-        <VerticalSeparator />
-        {renderLinkItem('POSITIONS', '/', false)}
-        <VerticalSeparator />
-        {renderLinkItem('ABOUT', '/', false)}
-        <VerticalSeparator />
-      </LinksContainer>
+      <HeaderAbsoluteContainer>
+        <LinksContainer>
+          <VerticalSeparator />
+          {renderLinkItem("PRODUCTS", "/", true)}
+          <VerticalSeparator />
+          {renderLinkItem("POSITIONS", "/", false)}
+          <VerticalSeparator />
+          {renderLinkItem("ABOUT", "/", false)}
+          <VerticalSeparator />
+        </LinksContainer>
+      </HeaderAbsoluteContainer>
 
       {/* WALLET */}
       <WalletContainer>
         <Indicator connected={walletConnected} />
-        <LinkText>0x573B...	C65F</LinkText>
+        <LinkText>0x573B... C65F</LinkText>
       </WalletContainer>
 
       {/* MOBILE MENU */}
       <MobileOnly>
         <MenuButton onToggle={onToggleMenu} isOpen={isMenuOpen} />
         <MobileOverlayMenu
-          onClick={() => console.log('oopsie do')}
           className="d-flex flex-column align-items-center justify-content-center"
-          style={mobileOverlayStyle}
+          isMenuOpen={isMenuOpen}
         >
-          {renderLinkItem('PRODUCTS', '/', true)}
-          {renderLinkItem('POSITIONS', '/', false)}
-          {renderLinkItem('ABOUT', '/', false)}
+          {renderLinkItem("PRODUCTS", "/", true)}
+          {renderLinkItem("POSITIONS", "/", false)}
+          {renderLinkItem("ABOUT", "/", false)}
         </MobileOverlayMenu>
       </MobileOnly>
     </HeaderContainer>
