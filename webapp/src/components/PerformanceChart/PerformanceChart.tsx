ade5d09a5a3c0c7e175dcde4ea2bff8df51e7094
kenchangh <guanhao3797@gmail.com>
2021-03-23 21:59:54 +0800

    working gradient

--- webapp/src/components/PerformanceChart/PerformanceChart.tsx
+++ webapp/src/components/PerformanceChart/PerformanceChart.tsx
@@ -1,22 +1,5 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Line } from "react-chartjs-2";
-
-const data = {
-  labels: ["1", "2", "3", "4", "5", "6"],
-  datasets: [
-    {
-      data: [12, 19, 3, 5, 2, 3],
-      type: "line",
-      pointRadius: 0,
-      tension: 0,
-      fill: "start",
-      borderColor: "#16CEB9",
-      //   background:
-      //     "linear-gradient(180deg, rgba(121, 255, 203, 0.24) 0%, rgba(121, 255, 203, 0) 100%)",
-      //   backgroundColor: "rgba(121, 255, 203, 0.24)",
-    },
-  ],
-};
 
 const options = {
   title: { display: false },
@@ -32,9 +15,33 @@ const options = {
 };
 
 const PerformanceChart = () => {
+  const getData = useCallback((canvas: any) => {
+    const ctx = canvas.getContext("2d");
+    let gradient = ctx.createLinearGradient(0, 0, 0, 250);
+    gradient.addColorStop(0, "rgba(121, 255, 203, 0.24)");
+    gradient.addColorStop(1, "rgba(121, 255, 203, 0)");
+
+    const dataset = [3, 5, 2, 3, 5, 4, 3, 1, 4, 5, 7, 8.5, 8, 7.5];
+
+    return {
+      labels: dataset.map((_, index) => (index + 1).toString()),
+      datasets: [
+        {
+          data: dataset,
+          type: "line",
+          pointRadius: 0,
+          tension: 0,
+          fill: "start",
+          borderColor: "#16CEB9",
+          backgroundColor: gradient,
+        },
+      ],
+    };
+  }, []);
+
   return (
     <div>
-      <Line data={data} options={options}></Line>
+      <Line data={getData} options={options}></Line>
     </div>
   );
 };
