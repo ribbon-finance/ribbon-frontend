3899ad64cfaa58c89526207a7cd73adc116f33eb
kenchangh <guanhao3797@gmail.com>
2021-03-23 15:36:53 +0800

    working sass compilation

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,7 +1,6 @@
-import Header from "./components/Header";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
+import Header from "./components/Header";
 import DepositPage from "./pages/DepositPage";
 
 function App() {
