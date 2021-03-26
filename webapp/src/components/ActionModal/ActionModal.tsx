e5ad95686475a36c53d7d873d3f6f172504c4b70
kenchangh <guanhao3797@gmail.com>
2021-03-27 01:14:22 +0800

    basic transition done

--- webapp/src/components/ActionModal/ActionModal.tsx
+++ webapp/src/components/ActionModal/ActionModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
 import styled from "styled-components";
 import { BigNumber } from "ethers";
@@ -24,6 +24,21 @@ const CloseButton = styled.i`
   paddingright: 20px;
 `;
 
+type PreviewStep = 0;
+type ConfirmationStep = 1;
+type SubmittedStep = 2;
+type Steps = PreviewStep | ConfirmationStep | SubmittedStep;
+
+const STEPS: {
+  previewStep: PreviewStep;
+  confirmationStep: ConfirmationStep;
+  submittedStep: SubmittedStep;
+} = {
+  previewStep: 0,
+  confirmationStep: 1,
+  submittedStep: 2,
+};
+
 const ActionModal: React.FC<{
   actionType: ActionType;
   show: boolean;
@@ -32,20 +47,60 @@ const ActionModal: React.FC<{
   positionAmount: BigNumber;
   actionParams: ActionParams;
 }> = ({ actionType, show, onClose, amount, positionAmount, actionParams }) => {
+  const [step, setStep] = useState<Steps>(STEPS.previewStep);
+
   const actionWord = actionType === ACTIONS.deposit ? "Deposit" : "Withdrawal";
 
   const vault = useVault();
 
   const handleClickActionButton = async () => {
     if (vault !== null) {
-      await vault.depositETH({ value: amount });
+      setStep(STEPS.confirmationStep);
+      const res = await vault.depositETH({ value: amount });
     }
   };
+
+  const titles = {
+    0: `${actionWord} Preview`,
+    1: "Confirm Transaction",
+    2: "Transaction Submitted",
+  };
+
+  let stepComponent;
+
+  switch (step) {
+    case STEPS.previewStep:
+      stepComponent = (
+        <PreviewStep
+          actionType={actionType}
+          amount={amount}
+          positionAmount={positionAmount}
+          actionParams={actionParams}
+          onClickActionButton={handleClickActionButton}
+        ></PreviewStep>
+      );
+      break;
+
+    case STEPS.confirmationStep:
+      stepComponent = <div>test</div>;
+      break;
+
+    default:
+      stepComponent = (
+        <PreviewStep
+          actionType={actionType}
+          amount={amount}
+          positionAmount={positionAmount}
+          actionParams={actionParams}
+          onClickActionButton={handleClickActionButton}
+        ></PreviewStep>
+      );
+  }
 
   return (
     <BaseModal show={show} onHide={onClose} centered>
       <ActionModalHeader closeButton={false}>
-        <ModalTitle>{actionWord} Preview</ModalTitle>
+        <ModalTitle>{titles[step]}</ModalTitle>
 
         <CloseButton
           onClick={onClose}
@@ -54,13 +109,7 @@ const ActionModal: React.FC<{
       </ActionModalHeader>
 
       <Modal.Body className="d-flex flex-column align-items-center">
-        <PreviewStep
-          actionType={actionType}
-          amount={amount}
-          positionAmount={positionAmount}
-          actionParams={actionParams}
-          onClickActionButton={handleClickActionButton}
-        ></PreviewStep>
+        {stepComponent}
       </Modal.Body>
     </BaseModal>
   );
