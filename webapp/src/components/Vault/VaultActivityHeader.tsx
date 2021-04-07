45d75095f24bb08a9edb3ffe34d2c19626bbf0aa
Chan Jing Hong <jinghongchan@gmail.com>
2021-04-07 15:04:54 +0800

    Add Sort Filter

--- webapp/src/components/Vault/VaultActivityHeader.tsx
+++ webapp/src/components/Vault/VaultActivityHeader.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import styled from "styled-components";
 
 import { BaseButton, Title } from "../../designSystem";
 import colors from "../../designSystem/colors";
+import sizes from "../../designSystem/sizes";
+import theme from "../../designSystem/theme";
+import useOutsideAlerter from "../../hooks/useOutsideAlerter";
+import { capitalize } from "../../utils/text";
 import ButtonArrow from "../Common/ButtonArrow";
-import { SortBy, ActivityFilter } from "./types";
+import { SortBy, ActivityFilter, activityFilters, sortByList } from "./types";
 
 const Header = styled(Title)`
   font-size: 18px;
@@ -35,6 +39,62 @@ const FilterButtonText = styled(Title)`
   text-transform: uppercase;
 `;
 
+const FilterDropdownMenu = styled.div<{ isOpen: boolean }>`
+  ${(props) =>
+    props.isOpen
+      ? `
+          position: absolute;
+          right: 0px;
+          top: 48px;
+          width: fit-content;
+          background-color: ${colors.backgroundDarker};
+          border-radius: ${theme.border.radius};
+        `
+      : `
+          display: none;
+        `}
+`;
+
+const MenuItem = styled.div`
+  padding: 8px 16px;
+  padding-right: 38px;
+  opacity: 1;
+  display: flex;
+  align-items: center;
+
+  &:first-child {
+    padding-top: 16px;
+  }
+
+  &:last-child {
+    padding-bottom: 16px;
+  }
+
+  &:hover {
+    span {
+      color: ${colors.primaryText};
+    }
+  }
+
+  @media (max-width: ${sizes.md}px) {
+    margin: unset;
+    && {
+      padding: 28px;
+    }
+  }
+`;
+
+const MenuItemText = styled(Title)`
+  color: ${colors.primaryText}A3;
+  white-space: nowrap;
+  font-size: 14px;
+  line-height: 20px;
+
+  @media (max-width: ${sizes.md}px) {
+    font-size: 24px;
+  }
+`;
+
 interface VaultActivityHeaderProps {
   activityFilter: ActivityFilter;
   setActivityFilter: React.Dispatch<React.SetStateAction<ActivityFilter>>;
@@ -50,11 +110,28 @@ const VaultActivityHeader: React.FC<VaultActivityHeaderProps> = ({
 }) => {
   const [activityFilterOpen, setActivityFilterOpen] = useState(false);
   const [sortByOpen, setSortByOpen] = useState(false);
+  const activityFilterRef = useRef<HTMLDivElement>(null);
+  const sortByFilterRef = useRef<HTMLDivElement>(null);
+  useOutsideAlerter(activityFilterRef, () => {
+    setActivityFilterOpen(false);
+  });
+  useOutsideAlerter(sortByFilterRef, () => {
+    setSortByOpen(false);
+  });
+
+  const renderMenuItem = useCallback(
+    (title: string, onClick: () => void) => (
+      <MenuItem onClick={onClick} role="button">
+        <MenuItemText>{title}</MenuItemText>
+      </MenuItem>
+    ),
+    []
+  );
 
   return (
     <div className="d-flex align-items-center">
       <Header>Vault Activity</Header>
-      <Filter>
+      <Filter ref={activityFilterRef}>
         <FilterButton
           role="button"
           onClick={() => {
@@ -65,8 +142,16 @@ const VaultActivityHeader: React.FC<VaultActivityHeaderProps> = ({
             {activityFilter} <ButtonArrow isOpen={activityFilterOpen} />
           </FilterButtonText>
         </FilterButton>
+        <FilterDropdownMenu isOpen={activityFilterOpen}>
+          {activityFilters.map((filterOption) =>
+            renderMenuItem(capitalize(filterOption), () => {
+              setActivityFilter(filterOption);
+              setActivityFilterOpen(false);
+            })
+          )}
+        </FilterDropdownMenu>
       </Filter>
-      <Filter>
+      <Filter ref={sortByFilterRef}>
         <FilterButton
           role="button"
           onClick={() => {
@@ -77,6 +162,14 @@ const VaultActivityHeader: React.FC<VaultActivityHeaderProps> = ({
             {sortBy} <ButtonArrow isOpen={sortByOpen} />
           </FilterButtonText>
         </FilterButton>
+        <FilterDropdownMenu isOpen={sortByOpen}>
+          {sortByList.map((sortOption) =>
+            renderMenuItem(capitalize(sortOption), () => {
+              setSortBy(sortOption);
+              setSortByOpen(false);
+            })
+          )}
+        </FilterDropdownMenu>
       </Filter>
     </div>
   );
