5ccf9d7b55bf6a31729042c1996d452fec50899e
Chan Jing Hong <jinghongchan@gmail.com>
2021-04-22 19:48:20 +0800

    Add polyfill fixing non-smooth scroll on safari mobile

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,16 @@
 import { Web3ReactProvider } from "@web3-react/core";
+import { useEffect } from "react";
+import smoothscroll from "smoothscroll-polyfill";
 
 import RootApp from "./components/RootApp";
 import { Web3ContextProvider } from "./hooks/web3Context";
 import { getLibrary } from "./utils/getLibrary";
 
 function App() {
+  useEffect(() => {
+    smoothscroll.polyfill();
+  }, []);
+
   return (
     <Web3ContextProvider>
       <Web3ReactProvider getLibrary={getLibrary}>
