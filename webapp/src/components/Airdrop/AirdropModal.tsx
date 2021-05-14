4562eb6330278647c5872bb9cd9f2b79433adc0a
Chan Jing Hong <jinghongchan@gmail.com>
2021-05-14 12:48:06 +0800

    Add connect wallet airdrop view

--- webapp/src/components/Airdrop/AirdropModal.tsx
+++ webapp/src/components/Airdrop/AirdropModal.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { Modal } from "react-bootstrap";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { useWeb3React } from "@web3-react/core";
 
 import {
@@ -15,12 +15,17 @@ import {
 import Logo from "shared/lib/assets/icons/logo";
 import colors from "shared/lib/designSystem/colors";
 import { ExternalIcon } from "shared/lib/assets/icons/icons";
-import { ConnectWalletButton } from "shared/lib/components/Common/buttons";
+import {
+  ActionButton,
+  ConnectWalletButton,
+} from "shared/lib/components/Common/buttons";
 import useConnectWalletModal from "../../hooks/useConnectWalletModal";
 
 const StyledModal = styled(BaseModal)`
-  .modal-content {
+  .modal-dialog {
     max-width: 343px;
+    margin-left: auto;
+    margin-right: auto;
   }
 `;
 
@@ -34,6 +39,20 @@ const ContentColumn = styled.div<{ marginTop?: number }>`
   justify-content: center;
   margin-top: ${(props) => (props.marginTop ? props.marginTop : 24)}px;
   width: 100%;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const RotatingLogo = styled(Logo)`
+  animation: ${rotate} 4s linear infinite;
 `;
 
 const UnclaimLabel = styled(Subtitle)`
@@ -54,10 +73,12 @@ const UnclaimData = styled(Title)<{ variant: "big" | "small" }>`
       case "big":
         return `
           font-size: 96px;
+          line-height: 96px;
         `;
       case "small":
         return `
           font-size: 64px;
+          line-height: 64px;
         `;
     }
   }}
@@ -86,13 +107,20 @@ const AirdropModal: React.FC<AirdropModalProps> = ({ show, onClose }) => {
   const { account } = useWeb3React();
   const [, setShowConnectModal] = useConnectWalletModal();
 
+  const renderTopLogo = useCallback(
+    () => (
+      <ContentColumn marginTop={-24}>
+        <RotatingLogo height="64px" width="64px" />
+      </ContentColumn>
+    ),
+    []
+  );
+
   const content = useMemo(() => {
     if (!account) {
       return (
         <>
-          <ContentColumn marginTop={-24}>
-            <Logo height="64px" width="64px" />
-          </ContentColumn>
+          {renderTopLogo()}
           <ContentColumn>
             <UnclaimLabel>UNCLAIMED $RIBBON</UnclaimLabel>
           </ContentColumn>
@@ -127,16 +155,32 @@ const AirdropModal: React.FC<AirdropModalProps> = ({ show, onClose }) => {
       case "info":
         return (
           <>
-            <ContentColumn marginTop={-24}>
-              <Logo height="64px" width="64px" />
-            </ContentColumn>
+            {renderTopLogo()}
             <ContentColumn marginTop={56}>
               <UnclaimLabel>UNCLAIMED $RIBBON</UnclaimLabel>
+            </ContentColumn>
+            <ContentColumn marginTop={8}>
+              <UnclaimData variant="big">0</UnclaimData>
+            </ContentColumn>
+            <ContentColumn marginTop={56}>
+              <LearnMoreLink to="https://ribbon.finance/faq">
+                <PrimaryText>Read about $RIBBON</PrimaryText>
+                <LearnMoreIcon height="20px" width="20px" color="white" />
+              </LearnMoreLink>
+            </ContentColumn>
+            <ContentColumn marginTop={80}>
+              <ActionButton
+                onClick={() => setShowConnectModal(true)}
+                className="btn py-3 mb-2"
+              >
+                CLAIM $RIBBON
+              </ActionButton>
             </ContentColumn>
           </>
         );
     }
   }, [account, steps, setShowConnectModal]);
+
   return (
     <StyledModal show={show} onHide={onClose} centered backdrop={true}>
       <BaseModalHeader closeButton></BaseModalHeader>
