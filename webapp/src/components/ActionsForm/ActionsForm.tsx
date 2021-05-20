888e0d3bbb814444e4d5a744cedc082c18863fff
Chan Jing Hong <jinghongchan@gmail.com>
2021-05-20 18:06:28 +0800

    Fix max button does not clear insufficient balance error

--- webapp/src/components/ActionsForm/ActionsForm.tsx
+++ webapp/src/components/ActionsForm/ActionsForm.tsx
@@ -415,6 +415,8 @@ const ActionsForm: React.FC<ActionFormVariantProps & FormStepProps> = ({
   const isInputNonZero = parseFloat(inputAmount) > 0;
 
   const handleClickMax = async () => {
+    // Clear error
+    setError("none");
     if (!isLoadingData && connected && vault && account) {
       if (isDeposit && gasPrice !== "") {
         const gasLimit = isDeposit
