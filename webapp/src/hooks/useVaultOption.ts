ad2a6ac8e9560c5caf7c7c302b739ad4acc19386
Chan Jing Hong <jinghongchan@gmail.com>
2021-08-09 22:24:47 +0800

    Update useVaultOption to avoid nested tenary operator

--- webapp/src/hooks/useVaultOption.ts
+++ webapp/src/hooks/useVaultOption.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 import { useRouteMatch } from "react-router-dom";
 
 import {
@@ -15,56 +15,32 @@ const useVaultOption = () => {
   const matchv2 = useRouteMatch<{ vaultSymbol: string }>(
     "/v2/theta-vault/:vaultSymbol"
   );
-  const [vaultOption, setVaultOption] = useState<VaultOptions | undefined>(
-    // Match v1
-    matchv1?.params.vaultSymbol &&
-      matchv1.params.vaultSymbol in VaultNameOptionMap
-      ? VaultNameOptionMap[matchv1?.params.vaultSymbol as VaultName]
-      : // Match V2
-      matchv2?.params.vaultSymbol &&
-        matchv2.params.vaultSymbol in VaultNameOptionMap
-      ? VaultNameOptionMap[matchv2?.params.vaultSymbol as VaultName]
-      : undefined
-  );
-  const [vaultVersion, setVaultVersion] = useState<VaultVersion>(
-    // Match v1
-    matchv1?.params.vaultSymbol &&
-      matchv1.params.vaultSymbol in VaultNameOptionMap
-      ? "v1"
-      : // Match V2
-      matchv2?.params.vaultSymbol &&
-        matchv2.params.vaultSymbol in VaultNameOptionMap
-      ? "v2"
-      : // Default version
-        "v1"
-  );
-
-  useEffect(() => {
+  const [vaultOption, vaultVersion] = useMemo((): [
+    VaultOptions | undefined,
+    VaultVersion
+  ] => {
     if (
-      matchv1 &&
-      matchv1.params &&
+      matchv1?.params.vaultSymbol &&
       matchv1.params.vaultSymbol in VaultNameOptionMap
     ) {
-      setVaultOption(
-        VaultNameOptionMap[matchv1.params.vaultSymbol as VaultName]
-      );
-      setVaultVersion("v1");
-      return;
+      return [
+        VaultNameOptionMap[matchv1?.params.vaultSymbol as VaultName],
+        "v1",
+      ];
     }
 
     if (
-      matchv2 &&
-      matchv2.params &&
+      matchv2?.params.vaultSymbol &&
       matchv2.params.vaultSymbol in VaultNameOptionMap
     ) {
-      setVaultOption(
-        VaultNameOptionMap[matchv2.params.vaultSymbol as VaultName]
-      );
-      setVaultVersion("v2");
-      return;
+      return [
+        VaultNameOptionMap[matchv2?.params.vaultSymbol as VaultName],
+        "v2",
+      ];
     }
 
-    setVaultOption(undefined);
+    /** Default value */
+    return [undefined, "v1"];
   }, [matchv1, matchv2]);
 
   return { vaultOption, vaultVersion };
