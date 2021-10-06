ec7495bcfc4a740a003cc923526225942d6bf423
Chan Jing Hong <jinghongchan@gmail.com>
2021-10-06 21:29:47 +0800

    Optimize fetch user asset balance

--- shared/src/hooks/vaultDataContext.tsx
+++ shared/src/hooks/vaultDataContext.tsx
@@ -1,23 +1,34 @@
 import React, { ReactElement, useContext } from "react";
 
-import { VaultOptions } from "../constants/constants";
+import {
+  getAssets,
+  getDisplayAssets,
+  VaultOptions,
+} from "../constants/constants";
 import {
   defaultV2VaultData,
   VaultData,
   V2VaultData,
   defaultVaultData,
 } from "../models/vault";
+import { getAssetDecimals } from "../utils/asset";
+import useFetchAssetBalanceData, {
+  defaultUserAssetBalanceData,
+  UserAssetBalanceData,
+} from "./useFetchAssetBalanceData";
 import useFetchV2VaultData from "./useFetchV2VaultData";
 import useFetchVaultData from "./useFetchVaultData";
 
 export type VaultDataContextType = {
   v1: VaultData;
   v2: V2VaultData;
+  assetBalance: UserAssetBalanceData;
 };
 
 export const VaultDataContext = React.createContext<VaultDataContextType>({
   v1: defaultVaultData,
   v2: defaultV2VaultData,
+  assetBalance: defaultUserAssetBalanceData,
 });
 
 export const useVaultData = (vault: VaultOptions) => {
@@ -25,7 +36,14 @@ export const useVaultData = (vault: VaultOptions) => {
 
   return {
     ...contextData.v1.responses[vault],
-    status: contextData.v1.loading ? "loading" : "success",
+    asset: getAssets(vault),
+    displayAsset: getDisplayAssets(vault),
+    decimals: getAssetDecimals(getAssets(vault)),
+    userAssetBalance: contextData.assetBalance.data[getAssets(vault)],
+    status:
+      contextData.v1.loading || contextData.assetBalance.loading
+        ? "loading"
+        : "success",
   };
 };
 
@@ -42,8 +60,14 @@ export const useV2VaultData = (vault: VaultOptions) => {
   const contextData = useContext(VaultDataContext);
 
   return {
-    data: contextData.v2.responses[vault],
-    loading: contextData.v2.loading,
+    data: {
+      ...contextData.v2.responses[vault],
+      asset: getAssets(vault),
+      displayAsset: getDisplayAssets(vault),
+      decimals: getAssetDecimals(getAssets(vault)),
+      userAssetBalance: contextData.assetBalance.data[getAssets(vault)],
+    },
+    loading: contextData.v2.loading || contextData.assetBalance.loading,
   };
 };
 
@@ -52,12 +76,14 @@ export const VaultDataContextProvider: React.FC<{ children: ReactElement }> = ({
 }) => {
   const vaultData = useFetchVaultData();
   const v2VaultData = useFetchV2VaultData();
+  const assetBalance = useFetchAssetBalanceData();
 
   return (
     <VaultDataContext.Provider
       value={{
         v1: vaultData,
         v2: v2VaultData,
+        assetBalance,
       }}
     >
       {children}
