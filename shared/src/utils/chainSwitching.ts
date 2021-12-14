7c96f1139b54eda278aa231d07e4ea356801b584
kenchangh <guanhao3797@gmail.com>
2021-12-14 15:27:06 +0800

    Fix comments

--- shared/src/utils/chainSwitching.ts
+++ shared/src/utils/chainSwitching.ts
@@ -2,23 +2,30 @@ import { providers } from "ethers";
 import { AVALANCHE_MAINNET_PARAMS } from "../constants/chainParameters";
 import { CHAINID } from "../constants/constants";
 
+// This error code indicates that the chain has not been added to MetaMask.
+const UNAVAILABLE_CHAIN_CODE = 4902;
+
+/**
+ * Function copied and modified from https://docs.metamask.io/guide/rpc-api.html#other-rpc-methods
+ * error code 4902 refers to chain being available in the wallet
+ */
 export const switchChains = async (
   provider: providers.Web3Provider,
   chainId: number
 ) => {
   const hexChainId = "0x" + chainId.toString(16);
 
+  if (!provider.provider.request) return;
   try {
-    await provider.provider.request!({
+    await provider.provider.request({
       method: "wallet_switchEthereumChain",
       params: [{ chainId: hexChainId }],
     });
   } catch (switchError: any) {
-    // This error code indicates that the chain has not been added to MetaMask.
-    if (switchError.code === 4902) {
+    if (switchError.code === UNAVAILABLE_CHAIN_CODE) {
       if (chainId === CHAINID.AVAX_MAINNET) {
         try {
-          await provider.provider.request!({
+          await provider.provider.request({
             method: "wallet_addEthereumChain",
             params: [AVALANCHE_MAINNET_PARAMS],
           });
