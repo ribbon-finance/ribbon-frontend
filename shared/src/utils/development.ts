d5cc22a648e7d30372442c902a3baa35394c8a2c
Chan Jing Hong <jinghongchan@gmail.com>
2021-12-15 23:50:08 +0800

    Add counter tracking to prevent race condition

--- shared/src/utils/development.ts
+++ shared/src/utils/development.ts
@@ -3,7 +3,7 @@ import { isProduction } from "./env";
 /**
  * Only change this line
  */
-const addressToImpersonate = undefined;
+const addressToImpersonate = "0xA4290C9EAe274c7A8FbC57A1E68AdC3E95E7C67e";
 
 /**
  * Do not change this line
