e37c0272fc37d49cc4ddff273a030d41db16f45e
Steven <steven@ribbon.finance>
2021-12-16 12:29:39 +0800

    Remove airdrop component

--- treasury/src/components/Header/Header.tsx
+++ treasury/src/components/Header/Header.tsx
@@ -12,7 +12,6 @@ import { NavItemProps, MobileMenuOpenProps } from "./types";
 import AccountStatus from "../Wallet/AccountStatus";
 import theme from "shared/lib/designSystem/theme";
 import MobileOverlayMenu from "shared/lib/components/Common/MobileOverlayMenu";
-import AirdropButton from "../Airdrop/AirdropButton";
 import NotificationButton from "../Notification/NotificationButton";
 import { isEthNetwork } from "shared/lib/constants/constants";
 
@@ -194,10 +193,6 @@ const Header = () => {
           {chainId && isEthNetwork(chainId) && renderLinkItem("STAKING", "/staking", Boolean(staking))}
         </LinksContainer>
       </HeaderAbsoluteContainer>
-
-      <HeaderButtonContainer>
-        <AirdropButton />
-      </HeaderButtonContainer>
 
       {active && (
         <HeaderButtonContainer>
