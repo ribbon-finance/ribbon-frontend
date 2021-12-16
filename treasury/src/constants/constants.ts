51dfecaced0b34ee4d5a286d2472a3ce49487685
Steven <steven@ribbon.finance>
2021-12-16 13:03:13 +0800

    Removed airdrop related items

--- treasury/src/constants/constants.ts
+++ treasury/src/constants/constants.ts
@@ -4,19 +4,6 @@ import {
   VaultOptions,
   VaultVersion,
 } from "shared/lib/constants/constants";
-import { Airdrop, AirdropBreakdown, AirdropProof } from "../models/airdrop";
-
-export const proof: AirdropProof = isDevelopment()
-  ? require("../data/proof-kovan.json")
-  : require("../data/proof.json");
-
-export const airdrop: Airdrop = isDevelopment()
-  ? require("../data/airdrop-kovan.json")
-  : require("../data/airdrop.json");
-
-export const breakdown: AirdropBreakdown = isDevelopment()
-  ? require("../data/breakdown-kovan.json")
-  : require("../data/breakdown.json");
 
 export const getVaultURI = (
   vaultOption: VaultOptions,
