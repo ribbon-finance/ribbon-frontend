615e1a8b7b7716eefc38264844e77c40c3a34276
Ken Chan <guanhao3797@gmail.com>
2021-12-28 12:49:57 +0800

    ken/avax-unconnected Showing avax vault stats on the unconnected state (#315)
    Co-authored-by: Ben Burns <abwburns@gmail.com>

--- shared/src/hooks/web3Context.tsx
+++ shared/src/hooks/web3Context.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useContext } from "react";
 import { ethers } from "ethers";
 import { BaseProvider } from "@ethersproject/providers";
+import { isAvaxNetwork } from "../constants/constants";
 import { CHAINID, NODE_URI } from "../utils/env";
 
 export type Web3ContextData = {
@@ -10,15 +11,21 @@ export type Web3ContextData = {
 // TODO: Fix this in the future
 // Right now we just default to ETH mainnet for the unconnected wallet state
 // This means that we do not show the AVAX vault's details on the app
-const defaultProvider = ethers.getDefaultProvider(
-  NODE_URI[CHAINID.ETH_MAINNET]
-);
+const defaultProvider = ethers.getDefaultProvider(NODE_URI[CHAINID.ETH_KOVAN]);
+const avaxProvider = ethers.getDefaultProvider(NODE_URI[CHAINID.AVAX_FUJI]);
 
 export const Web3Context = React.createContext<Web3ContextData>({
   provider: defaultProvider,
 });
+export const AvaxWeb3Context = React.createContext<Web3ContextData>({
+  provider: avaxProvider,
+});
 
-export const useWeb3Context = () => useContext(Web3Context);
+export const useWeb3Context = (chainId: CHAINID = CHAINID.ETH_MAINNET) => {
+  let context = Web3Context;
+  if (isAvaxNetwork(chainId)) context = AvaxWeb3Context;
+  return useContext(context);
+};
 
 export const Web3ContextProvider: React.FC<{ children: ReactElement }> = ({
   children,
