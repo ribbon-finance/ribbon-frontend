ca39a8ff374e0e9067de13d2c6326f6be9fb8904
kenchangh <guanhao3797@gmail.com>
2022-01-12 01:32:55 +0800

    do it in webapp

--- shared/src/constants/constants.ts
+++ shared/src/constants/constants.ts
@@ -65,11 +65,15 @@ export const RetailVaultList = [
   "rUSDC-ETH-P-THETA",
 ] as const;
 
-export const TreasuryVaultList = [
-  "rPERP-TSRY"
-] as const;
+export const TreasuryVaultList = ["rPERP-TSRY"] as const;
 
-const AllVaultOptions = [...RetailVaultList, ...TreasuryVaultList]
+export const SolanaVaultList = ["rSOL-THETA"];
+
+const AllVaultOptions = [
+  ...RetailVaultList,
+  ...TreasuryVaultList,
+  ...SolanaVaultList,
+];
 
 export type VaultOptions = typeof AllVaultOptions[number];
 const ProdExcludeVault: VaultOptions[] = [];
@@ -82,8 +86,8 @@ const PutThetaVault: VaultOptions[] = [
 export const VaultList: VaultOptions[] = isTreasury()
   ? TreasuryVaultList
   : !isProduction()
-    ? RetailVaultList
-    : RetailVaultList.filter((vault) => !ProdExcludeVault.includes(vault));
+  ? RetailVaultList
+  : RetailVaultList.filter((vault) => !ProdExcludeVault.includes(vault));
 
 export const GAS_LIMITS: {
   [vault in VaultOptions]: Partial<{
@@ -304,7 +308,7 @@ export const VaultNamesList = [
   "T-stETH-C",
   "T-AAVE-C",
   "T-AVAX-C",
-  "T-PERP-C"
+  "T-PERP-C",
 ] as const;
 export type VaultName = typeof VaultNamesList[number];
 export const VaultNameOptionMap: { [name in VaultName]: VaultOptions } = {
@@ -416,7 +420,7 @@ export const VaultAllowedDepositAssets: { [vault in VaultOptions]: Assets[] } =
     "rUSDC-ETH-P-THETA": ["USDC"],
     "rstETH-THETA": ["stETH", "WETH"],
     "ryvUSDC-ETH-P-THETA": ["USDC"],
-    "rPERP-TSRY": ["PERP"]
+    "rPERP-TSRY": ["PERP"],
   };
 
 export const VaultMaxDeposit: { [vault in VaultOptions]: BigNumber } = {
@@ -442,7 +446,8 @@ export const VaultMaxDeposit: { [vault in VaultOptions]: BigNumber } = {
   "rAVAX-THETA": BigNumber.from(100000000).mul(
     BigNumber.from(10).pow(getAssetDecimals(getAssets("rAVAX-THETA")))
   ),
-  "rPERP-TSRY": BigNumber.from(100000000).mul( // Cap still not decided
+  "rPERP-TSRY": BigNumber.from(100000000).mul(
+    // Cap still not decided
     BigNumber.from(10).pow(getAssetDecimals(getAssets("rPERP-TSRY")))
   ),
 };
