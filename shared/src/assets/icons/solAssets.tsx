918e3d3aa63cf2fc1ded30e13c59fee010f5716e
kenchangh <guanhao3797@gmail.com>
2022-01-12 17:09:51 +0800

    replace useWeb3React

--- shared/src/assets/icons/solAssets.tsx
+++ shared/src/assets/icons/solAssets.tsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 
 type SVGProps = React.SVGAttributes<SVGElement>;
 
--- shared/src/hooks/useWeb3Wallet.ts
+++ shared/src/hooks/useWeb3Wallet.ts
@@ -1,39 +1,101 @@
 import { useWeb3React as useWeb3ReactEthereum } from "@web3-react/core";
+import { AbstractConnector } from "@web3-react/abstract-connector";
 import { CHAINS } from "../store/types";
 import { useSelectedChain } from "./useSelectedChain";
+import {
+  getWalletConnectConnector,
+  injectedConnector,
+  walletlinkConnector,
+} from "../utils/connectors";
+import { providers } from "ethers";
+import { useCallback } from "react";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
-interface Web3Wallet {
+interface Web3WalletData {
   chainId: number | undefined;
   active: boolean;
+  activate: (wallet: Wallet) => Promise<void>;
+  deactivate: () => Promise<void>;
   account: string | null | undefined;
-  library: unknown;
+  ethereumProvider: providers.Web3Provider | undefined;
 }
 
-const useWeb3Wallet = (): Web3Wallet => {
+enum EthereumWallet {
+  Metamask = 1,
+  WalletConnect = 2,
+  WalletLink = 3,
+}
+const ETHEREUM_WALLETS = Object.values(EthereumWallet);
+
+enum SolanaWallet {
+  Phantom = 4,
+  Solflare = 5,
+}
+
+type Wallet = EthereumWallet | SolanaWallet;
+
+export const useWeb3Wallet = (): Web3WalletData => {
   const [chain] = useSelectedChain();
 
   const {
     chainId: chainIdEth,
     active: activeEth,
+    activate: activateEth,
     account: accountEth,
     library: libraryEth,
+    deactivate: deactivateEth,
+    connector: connectorEth,
   } = useWeb3ReactEthereum();
+
+  const activate = useCallback(
+    async (wallet: Wallet) => {
+      if (isEthereumWallet(wallet)) {
+        const connector = ethereumConnectors[wallet as EthereumWallet]();
+        await activateEth(connector);
+      } else {
+        throw new Error("Wallet not supported");
+      }
+    },
+    [activateEth]
+  );
+
+  const deactivate = useCallback(async () => {
+    if (connectorEth) {
+      if (connectorEth instanceof WalletConnectConnector) {
+        await connectorEth.close();
+      }
+      deactivateEth();
+    }
+  }, [connectorEth, deactivateEth]);
 
   if (chain === CHAINS.SOLANA) {
     return {
       chainId: 99999,
       active: false,
       account: undefined,
-      library: undefined,
+      ethereumProvider: undefined,
+      activate: () => Promise.resolve(),
+      deactivate: () => Promise.resolve(),
     };
   }
 
   return {
     chainId: chainIdEth,
     active: activeEth,
+    activate,
+    deactivate,
     account: accountEth,
-    library: libraryEth,
+    ethereumProvider: libraryEth,
   };
 };
+
+const ethereumConnectors: Record<EthereumWallet, () => AbstractConnector> = {
+  [EthereumWallet.Metamask]: () => injectedConnector,
+  [EthereumWallet.WalletConnect]: getWalletConnectConnector,
+  [EthereumWallet.WalletLink]: () => walletlinkConnector,
+};
+
+const isEthereumWallet = (wallet: Wallet) =>
+  ETHEREUM_WALLETS.includes(wallet as unknown as string);
 
 export default useWeb3Wallet;
