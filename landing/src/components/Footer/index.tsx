c4465a986b39b219d13949999ac95c503965aea6
Leon Kho <leon@ribbon.finance>
2022-02-28 21:03:59 +0800

    Finalized footer refactor

--- landing/src/components/Footer/index.tsx
+++ landing/src/components/Footer/index.tsx
@@ -1,159 +1,149 @@
-import React, { useCallback } from "react";
+import React, { FC } from "react";
 import { Col, Row } from "react-bootstrap";
 import styled from "styled-components";
 
 import { BaseLink, Title } from "../../designSystem";
 import colors from "shared/lib/designSystem/colors";
-import sizes from "../../designSystem/sizes";
-import theme from "../../designSystem/theme";
 
-const FooterContainer = styled.div`
-  height: 52px;
+const FooterContainer = styled(Row)`
   width: 100%;
-  flex-wrap: nowrap;
-  border-top: ${theme.border.width} ${theme.border.style} ${colors.border};
-  display: none;
-
-  @media (min-width: ${sizes.xxl}px) {
-    display: flex;
-  }
-`;
-
-const LinksContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const LeftContainer = styled(LinksContainer)`
-  margin-right: auto;
 `;
 
 const LinkItem = styled.div`
-  padding: 0px 24px;
   opacity: 0.48;
+  margin-top: 40px;
+  text-align: center;
 
   &:hover {
     opacity: 1;
   }
 `;
 
-const MobileFooter = styled.div`
-  border-top: 1px solid ${colors.borderDark};
-
-  @media (min-width: ${sizes.xxl}px) {
-    display: none;
-  }
-`;
-
-const MobileFooterRow = styled(Row)`
-  margin-right: 0;
-  margin-left: 0;
-`;
-
-const MobileFooterCol = styled(Col)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0;
-
-  @media (max-width: ${sizes.xxl}px) {
-    padding-top: 27px;
-    padding-bottom: 27px;
-  }
-
-  @media (min-width: ${sizes.xxl}px) {
-    && {
-      max-width: unset;
-      flex: unset;
-    }
-  }
-
-  @media (min-width: ${sizes.xl}px) {
-    max-width: 50%;
-  }
-`;
-
 const LinkItemText = styled(Title)`
-  font-size: 14px;
+  font-size: 12px;
   line-height: 20px;
   white-space: nowrap;
 `;
 
-const DesktopFooter = () => {
-  const renderLinkItem = useCallback(
-    (title: string, to: string, external: boolean = false) => (
-      <MobileFooterCol xs={6}>
+const FooterTitle = styled.div`
+  font-size: 14px;
+  font-family: VCR;
+  text-transform: uppercase;
+  text-align: center;
+  color: ${colors.primaryText};
+`;
+
+interface Link {
+  title: string;
+  to: string;
+  external?: boolean;
+  download?: boolean;
+}
+
+const links = {
+  about: [
+    {
+      title: "FAQS",
+      to: "/faq",
+    },
+    {
+      title: "BLOG",
+      to: "https://medium.com/@ribbonfinance",
+      external: true,
+    },
+    {
+      title: "DOCS",
+      to: "https://docs.ribbon.finance",
+    },
+    {
+      title: "TERMS",
+      to: "/terms",
+    },
+    {
+      title: "POLICY",
+      to: "/policy",
+    },
+  ] as Array<Link>,
+  community: [
+    {
+      title: "GITHUB",
+      to: "https://github.com/ribbon-finance",
+      external: true,
+    },
+    {
+      title: "DISCORD",
+      to: "http://discord.ribbon.finance",
+      external: true,
+    },
+    {
+      title: "TWITTER",
+      to: "https://twitter.com/ribbonfinance",
+      external: true,
+    },
+    {
+      title: "MEDIA KIT",
+      to: "/ribbon_media_kit.zip",
+      external: true,
+      download: true,
+    },
+  ] as Array<Link>,
+  data: [
+    {
+      title: "SNAPSHOT",
+      to: "https://snapshot.org/#/rbn.eth",
+      external: true,
+    },
+    {
+      title: "TOKEN TERMINAL",
+      to: "https://www.tokenterminal.com/terminal/projects/ribbon-finance",
+      external: true,
+    },
+    {
+      title: "DEFI LLAMA",
+      to: "https://defillama.com/protocol/ribbon-finance",
+      external: true,
+    },
+  ] as Array<Link>,
+};
+
+const LinkList: FC<{ links: Array<Link> }> = ({ links }) => (
+  <>
+    {links.map((link) => {
+      return (
         <BaseLink
-          to={to}
-          target={external ? "_blank" : undefined}
-          rel={external ? "noreferrer noopener" : undefined}
+          to={link.to}
+          target={link.external ? "_blank" : undefined}
+          rel={link.external ? "noreferrer noopener" : undefined}
+          download={link.download}
         >
           <LinkItem>
-            <LinkItemText>{title}</LinkItemText>
+            <LinkItemText>{link.title}</LinkItemText>
           </LinkItem>
         </BaseLink>
-      </MobileFooterCol>
-    ),
-    []
-  );
+      );
+    })}
+  </>
+);
 
-  const mediaKit = (
-    <MobileFooterCol xs={6}>
-      <BaseLink to="/ribbon_media_kit.zip" target="_blank" download>
-        <LinkItem>
-          <LinkItemText>Media Kit</LinkItemText>
-        </LinkItem>
-      </BaseLink>
-    </MobileFooterCol>
-  );
-
+const Footer = () => {
   return (
     <>
-      <FooterContainer>
-        <LeftContainer>
-          {renderLinkItem("FAQS", "/faq")}
-          {renderLinkItem("BLOG", "https://medium.com/@ribbonfinance", true)}
-          {renderLinkItem("TERMS", "/terms")}
-          {renderLinkItem("POLICY", "/policy")}
-          {renderLinkItem("DOCS", "https://docs.ribbon.finance")}
-          {mediaKit}
-        </LeftContainer>
-        <div className="d-flex flex-row">
-          {renderLinkItem("SNAPSHOT", "https://snapshot.org/#/rbn.eth", true)}
-          {renderLinkItem(
-            "TOKEN TERMINAL",
-            "https://www.tokenterminal.com/terminal/projects/ribbon-finance",
-            true
-          )}
-          {renderLinkItem("DEFI PULSE", "https://defipulse.com/", true)}
-          {renderLinkItem("DISCORD", "http://discord.ribbon.finance", true)}
-          {renderLinkItem("TWITTER", "https://twitter.com/ribbonfinance", true)}
-          {renderLinkItem("GITHUB", "https://github.com/ribbon-finance", true)}
-        </div>
+      <FooterContainer fluid noGutters>
+        <Col xs={12} md={4}>
+          <FooterTitle>About</FooterTitle>
+          <LinkList links={links.about} />
+        </Col>
+        <Col xs={12} md={4}>
+          <FooterTitle>Community</FooterTitle>
+          <LinkList links={links.community} />
+        </Col>
+        <Col xs={12} md={4}>
+          <FooterTitle>Data</FooterTitle>
+          <LinkList links={links.data} />
+        </Col>
       </FooterContainer>
-
-      <MobileFooter>
-        <MobileFooterRow>
-          {renderLinkItem("FAQS", "/faq")}
-          {renderLinkItem("BLOG", "https://medium.com/@ribbonfinance", true)}
-          {renderLinkItem("TERMS", "/terms")}
-          {renderLinkItem("POLICY", "/policy")}
-          {renderLinkItem("DOCS", "https://docs.ribbon.finance")}
-          {renderLinkItem("SNAPSHOT", "https://snapshot.org/#/rbn.eth", true)}
-          {renderLinkItem("DEFI PULSE", "https://defipulse.com/", true)}
-          {renderLinkItem("DISCORD", "http://discord.ribbon.finance", true)}
-          {renderLinkItem("TWITTER", "https://twitter.com/ribbonfinance", true)}
-          {renderLinkItem("GITHUB", "https://github.com/ribbon-finance", true)}
-          {renderLinkItem(
-            "TOKEN TERMINAL",
-            "https://www.tokenterminal.com/terminal/projects/ribbon-finance",
-            true
-          )}
-          {mediaKit}
-        </MobileFooterRow>
-      </MobileFooter>
     </>
   );
 };
 
-export default DesktopFooter;
+export default Footer;
