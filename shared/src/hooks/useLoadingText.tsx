e0562f76b9a3103ea6af0e97c30fc2231ded4651
Ben <abwburns@gmail.com>
2022-03-03 00:11:59 -0500

    fix/loading-perf-enhancement Added key prop on LoadText to speed up React's reconciliation process (#421)

--- shared/src/hooks/useLoadingText.tsx
+++ shared/src/hooks/useLoadingText.tsx
@@ -5,7 +5,7 @@ const flashing = () => keyframes`
  100% {
     opacity: 0;
   }
-  
+
   50% {
     opacity: 1;
   }
@@ -28,7 +28,9 @@ const useLoadingText = (text: string = "Loading") => {
       <Dots>
         {/* Here we insert the delay values to map the respective dots */}
         {[0.3, 0.6, 0.9].map((value) => (
-          <Dot delay={value}>.</Dot>
+          <Dot key={value} delay={value}>
+            .
+          </Dot>
         ))}
       </Dots>
     </span>
