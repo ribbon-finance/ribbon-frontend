55785caace51b00924cc903a0701f972b59be870
Leon <37091946+leonkho@users.noreply.github.com>
2022-03-20 02:55:34 +0100

    Leon/ape frontend (#446)
    * Added frontend for APE Covered Call
    
    * Log
    
    * Log
    
    * Added condition

--- webapp/src/pages/DepositPage/DepositPage.tsx
+++ webapp/src/pages/DepositPage/DepositPage.tsx
@@ -258,10 +258,15 @@ const DepositPage = () => {
 
   return (
     <>
-      <Banner
-        message={"This is an experimental vault, please use at your own risk!"}
-        color={colors.red}
-      />
+      {vaultOption === "rAPE-THETA" && (
+        <Banner
+          message={
+            "This is an experimental vault, please use at your own risk!"
+          }
+          color={colors.red}
+        />
+      )}
+      
       <HeroSection
         depositCapBar={depositCapBar}
         vaultOption={vaultOption}
