e5a1d302d1a673d3cc91537c0d601f8fafc19443
Leon <37091946+leonkho@users.noreply.github.com>
2022-06-22 11:02:18 +0800

    Removed navlinks on header

--- treasury/src/components/Header/Header.tsx
+++ treasury/src/components/Header/Header.tsx
@@ -7,15 +7,12 @@ import HeaderLogo from "webapp/lib/components/Header/HeaderLogo";
 import colors from "shared/lib/designSystem/colors";
 import sizes from "shared/lib/designSystem/sizes";
 import { Title, BaseLink } from "shared/lib/designSystem";
-import MenuButton from "shared/lib/components/Common/MenuButton";
 import {
-  NavItemProps,
   MobileMenuOpenProps,
 } from "webapp/lib/components/Header/types";
-import AccountStatus from "webapp/lib/components/Wallet/AccountStatus";
 import theme from "shared/lib/designSystem/theme";
-import MobileOverlayMenu from "shared/lib/components/Common/MobileOverlayMenu";
-import NotificationButton from "../Notification/NotificationButton";
+import { ConnectWalletButton } from "shared/lib/components/Common/buttons";
+import AccountStatus from "webapp/lib/components/Wallet/AccountStatus";
 
 const HeaderContainer = styled.div<MobileMenuOpenProps>`
   height: ${theme.header.height}px;
@@ -70,116 +67,23 @@ const LogoContainer = styled.div`
   }
 `;
 
-const HeaderButtonContainer = styled.div`
-  display: flex;
-  margin-right: 8px;
-  z-index: 1;
-`;
-
-const HeaderAbsoluteContainer = styled.div`
-  position: absolute;
-  display: flex;
-  height: 100%;
-  width: 100%;
-  justify-content: center;
-
-  @media (max-width: ${sizes.lg}px) {
-    display: none;
-  }
-`;
-
-const LinksContainer = styled.div`
-  display: flex;
-`;
-
-const NavItem = styled.div<NavItemProps>`
-  display: flex;
-  align-items: center;
-  padding: 0px 28px;
-  height: 100%;
-  opacity: ${(props) => (props.isSelected ? "1" : "0.48")};
-
-  &:hover {
-    opacity: ${(props) => (props.isSelected ? theme.hover.opacity : "1")};
-  }
-
-  @media (max-width: ${sizes.lg}px) {
-    padding: 0px 0px 40px 48px;
-  }
-`;
-
-const NavLinkText = styled(Title)`
-  letter-spacing: 1.5px;
+const OpenTreasuryButton = styled(ConnectWalletButton)`
   font-size: 14px;
+  width: fit-content;
+  border: none;
+  padding: 12px 16px;
+  margin-right: 40px;
   line-height: 20px;
-
-  @media (max-width: ${sizes.lg}px) {
-    font-size: 24px;
-  }
-`;
-
-const SecondaryMobileNavItem = styled.div`
-  display: none;
-
-  @media (max-width: ${sizes.lg}px) {
-    display: flex;
-    padding: 0px 0px 24px 48px;
-  }
-`;
-
-const MobileOnly = styled.div`
-  display: none;
-
-  @media (max-width: ${sizes.lg}px) {
-    display: flex;
-  }
+  border-radius: 8px;
 `;
 
 const Header = () => {
-  const { active } = useWeb3Wallet();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const product = useRouteMatch({ path: "/", exact: true });
-  const portfolio = useRouteMatch({ path: "/portfolio", exact: true });
-  const treasury = useRouteMatch({ path: "/treasury", exact: false });
-
-  const onToggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const renderLinkItem = (
-    title: string,
-    to: string,
-    isSelected: boolean,
-    primary: boolean = true,
-    external: boolean = false
-  ) => {
-    return (
-      <BaseLink
-        to={to}
-        target={external ? "_blank" : undefined}
-        rel={external ? "noreferrer noopener" : undefined}
-        onClick={() => {
-          if (!external) setIsMenuOpen(false);
-        }}
-      >
-        {primary ? (
-          <NavItem isSelected={isSelected}>
-            <NavLinkText>{title}</NavLinkText>
-          </NavItem>
-        ) : (
-          <SecondaryMobileNavItem>
-            <Title fontSize={18} color={`${colors.primaryText}7A`}>
-              {title}
-            </Title>
-          </SecondaryMobileNavItem>
-        )}
-      </BaseLink>
-    );
-  };
+  // const product = useRouteMatch({ path: "/", exact: true });
+  // const portfolio = useRouteMatch({ path: "/portfolio", exact: true });
+  // const treasury = useRouteMatch({ path: "/treasury", exact: false });
 
   return (
     <HeaderContainer
-      isMenuOpen={isMenuOpen}
       className="d-flex align-items-center"
     >
       {/* LOGO */}
@@ -187,100 +91,9 @@ const Header = () => {
         <HeaderLogo />
       </LogoContainer>
 
-      {/* LINKS */}
-      <HeaderAbsoluteContainer>
-        <LinksContainer>
-          {renderLinkItem(
-            "TREASURY",
-            "/",
-            Boolean(product) || Boolean(treasury)
-          )}
-          {renderLinkItem("PORTFOLIO", "/portfolio", Boolean(portfolio))}
-        </LinksContainer>
-      </HeaderAbsoluteContainer>
-
-      {active && (
-        <HeaderButtonContainer>
-          <NotificationButton />
-        </HeaderButtonContainer>
-      )}
-
+      {/* TODO: Check if active page is root to determine which button to be rendered */}
       <AccountStatus variant="desktop" />
-
-      {/* MOBILE MENU */}
-      <MobileOnly>
-        <MenuButton onToggle={onToggleMenu} isOpen={isMenuOpen} />
-        <MobileOverlayMenu
-          className="flex-column align-items-center justify-content-center"
-          isMenuOpen={isMenuOpen}
-          onClick={onToggleMenu}
-          boundingDivProps={{
-            style: {
-              marginRight: "auto",
-            },
-          }}
-        >
-          {renderLinkItem(
-            "TREASURY",
-            "/",
-            Boolean(useRouteMatch({ path: "/", exact: true }))
-          )}
-          {renderLinkItem(
-            "PORTFOLIO",
-            "/portfolio",
-            Boolean(useRouteMatch({ path: "/portfolio", exact: true }))
-          )}
-          {renderLinkItem(
-            "DISCORD",
-            "http://tiny.cc/ribbon-discord",
-            false,
-            false,
-            true
-          )}
-          {renderLinkItem(
-            "TWITTER",
-            "https://twitter.com/ribbonfinance",
-            false,
-            false,
-            true
-          )}
-          {renderLinkItem(
-            "GITHUB",
-            "https://github.com/ribbon-finance",
-            false,
-            false,
-            true
-          )}
-          {renderLinkItem(
-            "FAQS",
-            "https://ribbon.finance/faq",
-            false,
-            false,
-            true
-          )}
-          {renderLinkItem(
-            "BLOG",
-            "https://medium.com/@ribbonfinance",
-            false,
-            false,
-            true
-          )}
-          {renderLinkItem(
-            "TERMS",
-            "https://ribbon.finance/terms",
-            false,
-            false,
-            true
-          )}
-          {renderLinkItem(
-            "POLICY",
-            "https://ribbon.finance/policy",
-            false,
-            false,
-            true
-          )}
-        </MobileOverlayMenu>
-      </MobileOnly>
+      <OpenTreasuryButton>Open Treasury</OpenTreasuryButton>
     </HeaderContainer>
   );
 };
