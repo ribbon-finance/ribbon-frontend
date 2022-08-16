9213190cc8b16d28e8b831f7743ebe7b41a7a067
Chan Jing Hong <jinghongchan@gmail.com>
2022-08-16 17:02:57 +0800

    WIP: useAirtable

--- shared/src/hooks/useAirtable.ts
+++ shared/src/hooks/useAirtable.ts
@@ -2,6 +2,7 @@ import Airtable from "airtable";
 import { FieldSet } from "airtable/lib/field_set";
 import Record from "airtable/lib/record";
 import dotenv from "dotenv";
+import { useEffect, useState } from "react";
 import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 // import { ScheduleItem } from "./types";
 
@@ -13,18 +14,46 @@ export interface ScheduleItem {
 
 dotenv.config();
 
-export interface AirtableValues {
-  strikePrice: number;
-  baseYield: number;
-  maxYield: number;
-}
-
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
   apiKey: process.env.REACT_APP_AIRTABLE_API_KEY,
 });
 
 const BASE_NAME = "Earn";
+
+const base = Airtable.base("appkUHzxJ1lehQTIt");
+
+export const useAirtable = () => {
+  const [schedules, setSchedule] = useState<ScheduleItem[]>();
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    // 1. When init load schedules
+    base(BASE_NAME)
+      .select({ view: "Grid view" })
+      .all()
+      .then((records) => {
+        const s: ScheduleItem[] = [];
+        records.forEach((record) => {
+          const fields = record.fields as unknown;
+          const item = fields as ScheduleItem;
+          s.push(item);
+        });
+        setSchedule(s);
+      })
+      .catch((e) => {
+        setSchedule([]);
+        setError("ERROR FETCHING");
+      });
+  }, []);
+
+  return {
+    loading: !schedules,
+    schedules,
+  };
+};
+
+// const { loading, schedules } = useAirtable();
 
 export const getAuctionSchedule = async () => {
   const base = Airtable.base("appkUHzxJ1lehQTIt");
