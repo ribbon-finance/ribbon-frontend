767a8a0209db550ce1993bb8a7fc41028dce85ec
Timothy <timongll@hotmail.com>
2022-08-22 17:52:34 +0800

    fix undefined rearn subgraph

--- shared/src/utils/env.ts
+++ shared/src/utils/env.ts
@@ -66,7 +66,8 @@ export const getSubgraphqlURI = () =>
   "https://api.thegraph.com/subgraphs/name/kenchangh/ribbon-finance-kovan";
 
 export const getSubgraphqlRearnURI = () =>
-  process.env.REACT_APP_SUBGRAPHQL_REARN_URL;
+  process.env.REACT_APP_SUBGRAPHQL_REARN_URL ||
+  "https://api.studio.thegraph.com/query/30834/ribbonearnvault2/anewohnwaiehnaowneh";
 
 export const supportedChainIds = isDevelopment()
   ? [CHAINID.ETH_KOVAN, CHAINID.AVAX_FUJI]
