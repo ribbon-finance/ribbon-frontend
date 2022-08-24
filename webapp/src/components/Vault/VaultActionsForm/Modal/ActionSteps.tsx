a75949742a65ecba8bb7768c85c118830d9fc153
Leon <37091946+leonkho@users.noreply.github.com>
2022-08-24 13:02:43 +0800

    Leon/sol pps (#533)
    * Flex SDK bump to 0.8.2
    
    * Added price per share for SOL redeemable mints, adjusted frontend withdrawal amount
    
    * Fixed SOL redemption amount
    
    * Removed redundant const
    
    * Abstracted amount by shares calculation
    
    * Migrated PPS into function directly

--- webapp/src/components/Vault/VaultActionsForm/Modal/ActionSteps.tsx
+++ webapp/src/components/Vault/VaultActionsForm/Modal/ActionSteps.tsx
@@ -16,7 +16,11 @@ import {
   getSolanaVaultInstance,
   isSolanaVault,
 } from "shared/lib/constants/constants";
-import { isETHVault } from "shared/lib/utils/vault";
+import {
+  getSOLPricePerShare,
+  getSOLAmountByShares,
+  isETHVault,
+} from "shared/lib/utils/vault";
 import { usePendingTransactions } from "shared/lib/hooks/pendingTransactionsContext";
 import useVaultActionForm from "../../../../hooks/useVaultActionForm";
 import { parseUnits } from "@ethersproject/units";
@@ -243,6 +247,7 @@ const ActionSteps: React.FC<ActionStepsProps> = ({
     if (vault !== null || (isSolanaVault(vaultOption) && client !== null)) {
       // check illegal state transition
       if (step !== STEPS.confirmationStep - 1) return;
+
       onChangeStep(STEPS.confirmationStep);
       try {
         let res: any;
@@ -329,9 +334,13 @@ const ActionSteps: React.FC<ActionStepsProps> = ({
                     switch (vaultActionForm.vaultOption) {
                       case "rSOL-THETA":
                         if (client) {
+                          const redeemableAmount = await getSOLAmountByShares(
+                            Number(vaultActionForm.inputAmount),
+                            decimals
+                          );
                           const txhash = await client.withdrawVault(
                             getSolanaVaultInstance(vaultOption),
-                            new anchor.BN(amountStr)
+                            new anchor.BN(redeemableAmount)
                           );
 
                           res = { hash: txhash };
