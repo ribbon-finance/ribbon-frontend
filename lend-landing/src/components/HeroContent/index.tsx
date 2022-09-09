19a7a4e5fe97b73b0a2e68efca159e3804a53ed1
Timothy <timongll@hotmail.com>
2022-09-09 06:43:55 -0700

    minor

--- lend-landing/src/components/HeroContent/index.tsx
+++ lend-landing/src/components/HeroContent/index.tsx
@@ -17,7 +17,7 @@ const ContentContainer = styled.div`
 const Panel = styled.div<{ marginLeft?: number; mobile?: boolean }>`
   display: flex;
   position: relative;
-  width: 150%;
+  width: 100%;
   height: 100%;
   overflow: hidden;
   margin-left: ${(props) =>
