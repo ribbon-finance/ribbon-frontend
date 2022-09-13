53297bd921060f3ba011e45a2e86f389d9f67b5f
Timothy <timongll@hotmail.com>
2022-09-13 12:58:05 -0700

    add video with clips

--- lend-landing/src/components/HeroContent/index.tsx
+++ lend-landing/src/components/HeroContent/index.tsx
@@ -7,6 +7,7 @@ import Marquee from "react-fast-marquee/dist";
 import sizes from "../../designSystem/sizes";
 import ReactPlayer from "react-player";
 import useScreenSize from "shared/lib/hooks/useScreenSize";
+import VideoBackground from "./VideoBackground";
 
 const ContentContainer = styled.div`
   position: relative;
@@ -20,20 +21,14 @@ const ContentContainer = styled.div`
 const Panel = styled.div<{ marginLeft?: number; marginRight?: number }>`
   display: flex;
   position: relative;
-  width: 100%;
+  width: calc(25% - 48px);
   height: 100%;
   overflow: hidden;
   z-index: 1000;
   margin-left: ${(props) =>
     props.marginLeft !== undefined ? `${props.marginLeft}px` : `40px`};
   margin-right: ${(props) =>
-    props.marginLeft !== undefined ? `${props.marginRight}px` : `0px`};
-  @media (max-width: ${sizes.lg}px) {
-    margin-left: ${(props) =>
-      props.marginLeft !== undefined ? `${props.marginLeft}px` : `16px`};
-    margin-right: ${(props) =>
-      props.marginLeft !== undefined ? `16px` : `0px`};
-  }
+    props.marginRight !== undefined ? `${props.marginRight}px` : `0px`};
 `;
 
 const SpecialText = styled(Title)<{ size: number }>`
@@ -78,86 +73,6 @@ const HeroHeaderMobileContainer = styled.div`
   }
 `;
 
-const Video1 = styled.div`
-  position: absolute;
-  width: 100%;
-  display: flex;
-  height: calc(50% - 16px);
-  background: grey;
-  overflow: hidden;
-`;
-
-const Video2 = styled.div`
-  position: absolute;
-  width: 100%;
-  display: flex;
-  height: calc(50% - 48px);
-  top: 0;
-  bottom: 0;
-  margin: auto;
-  overflow: hidden;
-  background: grey;
-`;
-
-const Video3 = styled.div`
-  position: absolute;
-  width: 100%;
-  display: flex;
-  height: 104px;
-  bottom: 40px;
-  margin: auto;
-  @media (max-width: ${sizes.lg}px) {
-    bottom: 16px;
-  }
-  background: grey;
-`;
-
-const Video4 = styled.div`
-  position: absolute;
-  width: 100%;
-  display: flex;
-  height: calc(50% - 48px);
-  top: 0;
-  bottom: 0;
-  margin: auto;
-`;
-
-const Video4Content = styled.div`
-  position: relative;
-  height: 40%;
-  width: 100%;
-  background: grey;
-`;
-
-const Container = styled.div<{ backgroundColor?: string }>`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-  z-index: 10000;
-  background: black;
-`;
-
-const FloatingBackgroundContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  z-index: 1000;
-`;
-
-const BackgroundContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  z-index: 1000;
-  background: black;
-`;
-
 interface HeroHeaderInterface {
   clockwise: boolean;
 }
@@ -168,6 +83,7 @@ const HeroHeader: React.FC<HeroHeaderInterface> = ({ children, clockwise }) => {
       <AnimatePresence exitBeforeEnter>
         <motion.div
           transition={{
+            delay: 0.5,
             duration: 0.25,
             type: "keyframes",
             ease: "easeOut",
@@ -202,6 +118,7 @@ const HeroHeaderMobile: React.FC = ({ children }) => {
       <AnimatePresence exitBeforeEnter>
         <motion.div
           transition={{
+            delay: 2,
             duration: 0.25,
             type: "keyframes",
             ease: "easeOut",
@@ -226,48 +143,24 @@ const HeroHeaderMobile: React.FC = ({ children }) => {
 };
 
 const HeroContent: React.FC = () => {
-  const { video } = useScreenSize();
-
   return (
     <ContentContainer>
-      {/* <FloatingBackgroundContainer>
-        <ReactPlayer
-          key="video-player"
-          url="https://player.vimeo.com/video/748213676?h=6ac437c1b3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-          playing={true}
-          width={video.width}
-          height={video.height}
-          style={{
-            minWidth: video.width,
-            minHeight: video.height,
-          }}
-          muted
-          loop
-        />
-      </FloatingBackgroundContainer> */}
+      <VideoBackground />
       <HeroHeaderMobile>
         <SpecialText size={152}>Ribbon Lend</SpecialText>
       </HeroHeaderMobile>
-      <Panel>
-        <HeroHeader clockwise={false}>
-          <SpecialText size={256}>Ribbon</SpecialText>
-        </HeroHeader>
-        <Video1 />
-      </Panel>
-      <Panel>
-        <Video2 />
-      </Panel>
-      <Panel>
-        <Video3 />
-      </Panel>
-      <Panel marginLeft={0} marginRight={40}>
-        <Video4>
-          <Video4Content />
-        </Video4>
-        <HeroHeader clockwise={true}>
-          <SpecialText size={256}>Lend&nbsp;&nbsp;</SpecialText>
-        </HeroHeader>
-      </Panel>
+      <div className="d-flex align-items-center justify-content-between w-100 h-100">
+        <Panel marginLeft={32} marginRight={50}>
+          <HeroHeader clockwise={false}>
+            <SpecialText size={256}>Ribbon</SpecialText>
+          </HeroHeader>
+        </Panel>
+        <Panel marginLeft={0} marginRight={32}>
+          <HeroHeader clockwise={true}>
+            <SpecialText size={256}>Lend&nbsp;&nbsp;</SpecialText>
+          </HeroHeader>
+        </Panel>
+      </div>
     </ContentContainer>
   );
 };
