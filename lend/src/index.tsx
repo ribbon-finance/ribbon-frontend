c4b201cefb73f0809076893689d3882a563c1d42
Leon <37091946+leonkho@users.noreply.github.com>
2022-09-14 18:07:19 +0800

    Removed lend dir, renamed lend-landing dir

--- lend/src/index.tsx
+++ lend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
