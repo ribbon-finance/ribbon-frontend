2601c2e657a06e506fb4de3a1df122ae57b33afc
jhh <cjinghong@users.noreply.github.com>
2022-10-07 13:32:09 +0800

    Jh/earn lm (#586)
    * added earn liq gauge
    
    * lint
    
    * fix null contract, move rearn liq card to top
    
    * change tooltip for stake cards
    
    * fix staking getContract
    
    * remove useEarnVaultContract and useV2VaultContract
    
    Co-authored-by: Timothy <timongll@hotmail.com>

--- shared/src/hooks/useVaultContract.ts
+++ shared/src/hooks/useVaultContract.ts
@@ -4,25 +4,33 @@ import {
   RibbonV2stETHThetaVault,
   RibbonV2stETHThetaVault__factory,
   RibbonV2ThetaVault,
+  RibbonEarnVault,
+  RibbonEarnVault__factory,
 } from "../codegen";
 import { RibbonV2ThetaVault__factory } from "../codegen/factories/RibbonV2ThetaVault__factory";
 import {
+  isEarnVault,
   isSolanaVault,
   VaultAddressMap,
   VaultOptions,
 } from "../constants/constants";
 import { useWeb3Context } from "./web3Context";
 
-export const getV2VaultContract = (
+export const getVaultContract = (
   library: any,
   vaultOption: VaultOptions,
   useSigner: boolean = true
 ) => {
-  if (!VaultAddressMap[vaultOption].v2) {
+  if (!VaultAddressMap[vaultOption].v2 && !VaultAddressMap[vaultOption].earn) {
     return null;
   }
   const provider = useSigner ? library.getSigner() : library;
-
+  if (isEarnVault(vaultOption)) {
+    return RibbonEarnVault__factory.connect(
+      VaultAddressMap[vaultOption].earn!,
+      provider
+    );
+  }
   switch (vaultOption) {
     case "rstETH-THETA":
       return RibbonV2stETHThetaVault__factory.connect(
@@ -37,19 +45,19 @@ export const getV2VaultContract = (
   }
 };
 
-const useV2VaultContract = (vaultOption: VaultOptions) => {
+const useVaultContract = (vaultOption: VaultOptions) => {
   const { active, library } = useWeb3React();
   const { provider } = useWeb3Context();
   const [vault, setVault] = useState<
-    RibbonV2ThetaVault | RibbonV2stETHThetaVault | null
+    RibbonV2ThetaVault | RibbonV2stETHThetaVault | RibbonEarnVault | null
   >(null);
 
   useEffect(() => {
     if (isSolanaVault(vaultOption)) return;
-    const vault = getV2VaultContract(library || provider, vaultOption, active);
+    const vault = getVaultContract(library || provider, vaultOption, active);
     setVault(vault);
   }, [active, library, provider, vaultOption]);
 
   return vault;
 };
-export default useV2VaultContract;
+export default useVaultContract;
