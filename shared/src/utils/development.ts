aad14444b77b75638c2044c414f4104810bb520d
Timothy <timongll@hotmail.com>
2023-04-04 14:36:21 +0800

    revert impersonate

--- shared/src/utils/development.ts
+++ shared/src/utils/development.ts
@@ -3,7 +3,7 @@ import { isProduction } from "./env";
 /**
  * Only change this line
  */
-const addressToImpersonate = "0xCeC909e109aF27C4220d8c0400Ec990126187Dce";
+const addressToImpersonate = undefined;
 
 /**
  * Do not change this line
