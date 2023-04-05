4eb676e05faf7ac85c46da8326e62e6195719700
Timothy <timongll@hotmail.com>
2023-04-05 13:49:40 +0800

    change banner to connect to bnb chain if not already

--- webapp/src/components/Banner/BNBBanner.tsx
+++ webapp/src/components/Banner/BNBBanner.tsx
@@ -1,13 +1,20 @@
 import Banner from "shared/lib/components/Banner/Banner";
+import { Chains } from "shared/lib/constants/constants";
 import colors from "shared/lib/designSystem/colors";
+import { useChain } from "shared/lib/hooks/chainContext";
+import useConnectWalletModal from "shared/lib/hooks/useConnectWalletModal";
 
 const BNBBanner: React.FC = () => {
+  const [chain] = useChain();
+  const isBnbChain = chain === Chains.Binance || chain === Chains.NotSelected;
+  const [, setShowConnectModal] = useConnectWalletModal();
   return (
     <Banner
       color={colors.asset.WBNB}
       message="BNB Covered Call Vault is now live"
-      linkURI="/v2/theta-vault/T-BNB-C"
-      linkText="Start Depositing"
+      onClick={() => (isBnbChain ? {} : setShowConnectModal(true))}
+      linkURI={isBnbChain ? "/v2/theta-vault/T-BNB-C" : "/"}
+      linkText={isBnbChain ? "Start Depositing" : "Connect to Binance Chain"}
     />
   );
 };
